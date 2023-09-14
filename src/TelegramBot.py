d93f0d96d4b59c46c29d5ce12b3238f58fc7e9d5
Colin Chen <colin.chen@live.com>
2023-09-14 20:58:52 +0800

    add telegram bot timeout exception handling

--- src/TelegramBot.py
+++ src/TelegramBot.py
@@ -1,9 +1,25 @@
 from telegram import Bot
+import telegram.error
+import asyncio
 
 class TelegramBot:
-    def __init__(self, api_token):
+    def __init__(self, api_token, retry_limit=3):
         self.api_token = api_token
+        self.retry_limit = retry_limit
 
     async def send_message(self, chat_id, text, parse_mode='None'):
         bot = Bot(token=self.api_token)
-        await bot.send_message(chat_id=chat_id, text=text, parse_mode=parse_mode)
+        retry_count = 0
+
+        while retry_count < self.retry_limit:
+            try:
+                await bot.send_message(chat_id=chat_id, text=text, parse_mode=parse_mode)
+                return  # Exit the loop if the message is sent successfully
+            except telegram.error.TimedOut as e:
+                print(f"Telegram API request timed out: {e}")
+                retry_count += 1
+                if retry_count < self.retry_limit:
+                    await asyncio.sleep(10)  # Wait for 10 seconds before retrying
+                else:
+                    print("Retry limit reached, giving up on sending the message")
+                    break
